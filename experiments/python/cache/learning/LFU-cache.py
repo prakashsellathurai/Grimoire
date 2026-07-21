985515ed396794068431358d24fa38e859a10f0c
Prakash Sellathurai <prakashsellathurai@gmail.com>
2026-07-21 13:03:09 +0530

    Update LFU-cache.py
    Signed-off-by: Prakash Sellathurai <prakashsellathurai@gmail.com>

--- experiments/python/cache/learning/LFU-cache.py
+++ experiments/python/cache/learning/LFU-cache.py
@@ -26,3 +26,91 @@
 # Dirty bit tracking
 #   keyMap: key -> value, freq, ttl, dirty:bool, version:int
 # 
+# 
+# hashmap based approach
+# Data structure :
+#    without cache invalidation key : value:frequency
+#   with cach invalidation : current_min_freq 
+#             frequency {keys}
+#              key : value,frequency
+#    maintaing keys in linkedlist to suppport O(1) insert update
+# 
+#  1. Hashmap key: (frequency,value)
+#  2.frequencies: [list of keys] (use set in python language)
+#  3.min_frequency 
+#  4 .capacity
+#  
+#  methods 
+#    internal insert(key, frequency, value)
+#       insert freq,value into key hashmap
+#          append key into frequency hahsmap list
+# 
+#    get key:
+#      1 if key not in cache return -1
+#      2  get frequency , value from key hahsmap
+#      3 get set from frequency hashmap and remove key
+#     4 if minf  == frwquency and above is empty 
+#            minf ==1
+#            delete frequency entry in frequencies
+#     5 insert(key, vaue,frequnecy + 1)
+#     return value
+#   put key value
+#       1.if capcity <= 0 exit
+#       2 if key exists in cache update frequency value in origincal hashmap 
+#           call hashmap
+#       3. if cache size matches capacity 
+#                     get minf of frequency and remove
+#       4 minf += 1
+#       insert key 1 ,value
+# 
+# 
+# 
+from collections import OrderedDict, defaultdict
+class LFUCache:
+    def __init__(self, capcity) -> None:
+        self.cap = capcity
+        self.key2val = {}
+        self.key2freq = {}
+        self.freq2key = defaultdict(OrderedDict)
+        self.minf= 0 
+
+    def get(self, key):
+        # check key not exists
+        if key not in self.key2val:
+            return -1
+        # update frequency by 
+        oldfreq = self.key2freq[key]
+        self.key2freq[key] = oldfreq + 1
+        self.freq2key[oldfreq].pop(key)
+        # edge case saves space if current frequency is emoty rempove
+        if not self.freq2key[oldfreq]:
+            del self.freq2key[oldfreq]
+        # add it to new frequency
+        self.freq2key[oldfreq + 1][key] = 1
+        # update minf frequency if it doesn't exits
+        if self.minf not in self.freq2key:
+            self.minf += 1
+        # return value
+        return self.key2val[key]
+
+    def put(self, key,value):
+        #check boundary cases
+        if self.cap <= 0:
+            return
+        # if key already in cache udpate value and call get to update 
+        # frequency
+        if key in self.key2val:
+            self.get(key)
+            self.key2val[key] = value
+            return
+
+        # if cap reaches limit pop from freq2 key via minf and delet from all three ds
+        if len(self.key2val) == self.cap:
+            delkey, _ = self.freq2key[self.minf].popitem(last=False)
+            del self.key2val[delkey]
+            del self.key2freq[delkey]  
+        # add entry if not exists
+        self.key2val[key] = value
+        self.key2freq[key] = 1
+        self.freq2key[1][key] = 1
+        self.minf = 1
